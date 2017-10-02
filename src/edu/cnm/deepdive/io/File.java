f18637b45ee8e842a7812fa93ea655ecfcf76020
anakissler <fhresak@gmail.com>
2017-10-02 09:39:57 -0600

    Add getMatrix method to parse strings into int[] rows.

--- src/edu/cnm/deepdive/io/File.java
+++ src/edu/cnm/deepdive/io/File.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,8 +13,8 @@ public class File {
 
   private static final String FILENAME = "largest-product-data - Copy.txt";
 
-  public static String [] getLines(String filename)
-    throws FileNotFoundException, IOException {
+  public static String[] getLines(String filename)
+      throws FileNotFoundException, IOException {
     try (
         FileInputStream inStream = new FileInputStream(filename);
         InputStreamReader reader = new InputStreamReader(inStream);
@@ -21,30 +22,43 @@ public class File {
     ) {
       List<String> lines = new LinkedList<>();
       //String line;
-     // while ((line = buffer.readLine()) != null) {
+      // while ((line = buffer.readLine()) != null) {
 
-    for (String line = buffer.readLine();
-        line !=null;
-        line = buffer.readLine()) {
-      line = line.trim();
-      if (!line.isEmpty()) {
-        lines.add(line);
+      for (String line = buffer.readLine();
+          line != null;
+          line = buffer.readLine()) {
+        line = line.trim();
+        if (!line.isEmpty()) {
+          lines.add(line);
+        }
       }
-    }
-    return lines.toArray(new String[0]);
+      return lines.toArray(new String[0]);
     }
   }
-  public static void main (String [] args) {
-    try {
-      for (String line : getLines(FILENAME)) {
-        System.out.println(line);
+
+  public static int[][] getMatrix(String[] lines, String delimiter) {
+    int[][] data = new int[lines.length][];
+    for (int i = 0; i < lines.length; i++) {
+      String[] parts = lines[i].split(delimiter);
+      int[] row = new int[parts.length];
+      for (int j = 0; j < parts.length; j++) {
+        row[j] = Integer.parseInt(parts[j]);
       }
-    } catch (IOException e) {
-      e.printStackTrace();
+      data[i] = row;
+    }
+    return data;
+  }
+
+  public static void main(String[] args)
+      throws IOException {
+    for (int[] row : getMatrix(getLines(FILENAME), "\\s+")) {
+      System.out.println(Arrays.toString(row));
     }
   }
 
 }
+
+
 
 
 
